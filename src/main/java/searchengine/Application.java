28fc6f02fc8cc9086fafa45c9d2253529119dca8
Murad Khalitov <darggun@gmail.com>
2024-03-04 19:23:54 +0300

    сменил имя проекта

--- src/main/java/searchengine/Application.java
+++ src/main/java/searchengine/Application.java
@@ -12,20 +12,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        try {
-            URL url = new URL("https://www.playback.ru");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setConnectTimeout(10000); // Устанавливаем таймаут соединения в 10 секунд
-            connection.setReadTimeout(10000); // Устанавливаем таймаут чтения в 10 секунд
-
-            //String baseUrl = "https://www.lenta.ru";
-            WebCrawler webCrawler = new WebCrawler(url.toString());
-            webCrawler.crawl();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
 
     }
 }
